d75b0adfdcfc06ac09d35293a66b4cc0491b3961
ashwinkk23 <ashwinkk.23@gmail.com>
2020-09-25 15:15:14 +0530

    Added License

--- pyFish/__init__.py
+++ pyFish/__init__.py
@@ -4,12 +4,24 @@
 
 """
 pyFish : a package to analyse timeseries data using stocastic differential equations
+Copyright (C) 2020 Theoritical Evolution and Ecology Lab (TEE Lab), IISc, Bengaluru
+
+This program is free software: you can redistribute it and/or modify it under the terms 
+of the GNU General Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see <https://www.gnu.org/licenses/>.
 """
 
 __title__ = "pyFish"
 __author__ = "Ashwin Karichannavar, (e-mail: ashwinkk.23@gmail.com)"
-__license__ = ""
-__copyright__ = ""
+__license__ = "GNU General Public License v3.0"
+__copyright__ = "Copyright (C) 2020 Theoritical Evolution and Ecology Lab (TEE Lab), IISc, Bengaluru"
 
 from pyFish.sde import SDE
 from pyFish.analysis import underlying_noise
--- setup.py
+++ setup.py
@@ -11,12 +11,30 @@ setup(
 	name='pyFish',
 	version='1.0',
 	description='Package to analyse stochastic time series data',
-	license="",
 	long_description=long_description,
+	long_description_content_type='text/markdown',
 	author='Ashwin Karichannavar',
 	author_email='ashwinkk.23@gmail.com',
-	url="https://github.com/ashwinkk23/pyFish",
-	packages=find_packages(),  #same as name
+	url="https://github.com/tee-lab/pyFish",
+	packages=find_packages(),
+	classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers, Researchers',
+        'Topic :: Data Analysis :: Stochastic Timeseries',
+
+        'License :: OSI Approved :: GNU General Public License v3.0',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
 	include_package_data=True,
 	package_data={'': ['data/extras/*.csv', 'data/pairwise/*.csv', 'data/ternary/*.csv', 'data/vector/*.csv']},
 	install_requires=INSTALL_REQUIREMENTS, #external packages as dependencies
