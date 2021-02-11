Fixed error in modified time step calculation method

pyFish/sde.py
@@ -86,7 +86,7 @@ class SDE():
 		if not self._isValidRange(r):
 			print('Warning : given order parameter range is not in valid (typle or list of length 2) format\nUsing range of data')
 			r = (min(X), max(X))
-		return np.arange(r[0], r[-1], inc), r
+		return np.arange(min(r), max(r), inc), r
 
 	def _drift_and_diffusion(self, X, t_int, dt, delta_t=1, inc=0.01):
 		"""