a16af421c37548b46e63f2df374a8dc700c73f35
ashwinkk23 <ashwinkk.23@gmail.com>
2022-05-05 17:44:30 +0530

    improved docs

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
 	name='pydaddy',
-	version='0.1.4',
+	version='0.1.5',
 	description='Package to analyse stochastic time series data',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
