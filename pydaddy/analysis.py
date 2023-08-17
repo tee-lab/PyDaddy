21b35b69e31681fb345f8628a1822cef85ad84d8
Arshed Nabeel <arshed.nabeel@gmail.com>
2023-08-17 15:17:02 +0530

    Bugfixes in noise diagnostics computation

--- pydaddy/analysis.py
+++ pydaddy/analysis.py
@@ -237,8 +237,8 @@ class UnderlyingNoise(SDE):
             except IndexError:
                 bin_y = len(bins_y) - 1
 
-            res_x[i] = (res_x[i] - avg_drift_x[bin_x, bin_y] * t_int) #/ ((avg_diff_x[bin_x, bin_y] + avg_diff_xy[bin_x, bin_y]) * np.sqrt(t_int))
-            res_y[i] = (res_y[i] - avg_drift_y[bin_x, bin_y] * t_int) #/ ((avg_diff_y[bin_x, bin_y] + avg_diff_xy[bin_x, bin_y]) * np.sqrt(t_int))
+            res_x[i] = (res_x[i] - avg_drift_x[bin_x, bin_y] * t_int) #/ np.sqrt((avg_diff_x[bin_x, bin_y] + avg_diff_xy[bin_x, bin_y]) * t_int)
+            res_y[i] = (res_y[i] - avg_drift_y[bin_x, bin_y] * t_int) #/ np.sqrt((avg_diff_y[bin_x, bin_y] + avg_diff_xy[bin_x, bin_y]) * t_int)
 
         return res_x / np.sqrt(Dt * t_int), res_y / np.sqrt(Dt * t_int)
 
